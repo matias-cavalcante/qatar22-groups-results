5acd2505ed9ad04e599d05445f16f2a574cfe31d
matias-cavalcante <matias.cavalcante93@gmail.com>
2022-12-02 22:51:45 +0000

    Organiced script in smaller functions and tested for different data

--- scrapData.py
+++ scrapData.py
@@ -1,48 +1,59 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pnd
-
-url = 'https://resultados.as.com/resultados/futbol/mundial/jornada/'
-page = requests.get(url)
 
 
-soup = BeautifulSoup(page.content, 'html.parser')
-
-# From here to line 18 we get all of the participant teams names
-countriesRaw = soup.find_all('span',
-                             class_='nombre-equipo')
-
-countriesClean = []
-
-for country in countriesRaw:
-    countriesClean.append(country.text)
-
-# From here to line 32 we get all results of phase 2
-resultsRaw = soup.find_all('a', class_='resultado')
-resultsFiltered = []
-resultsClean = []
-
-for res in resultsRaw:
-    resultsFiltered.append(res.text)
-
-for r in resultsFiltered:
-    numbers = r.split()
-    numbers.remove(numbers[1])
-    if len(numbers) == 2:
-        resultsClean.append(numbers)
-
-# Provisory list
-
-gamesList = []
-teams = 0
-
-for games in range(len(resultsClean)):
-    firstGame = []
-    firstGame.append(countriesClean[teams:(teams+2)])
-    firstGame.append(resultsClean[games])
-    gamesList.append(firstGame)
-    teams = teams + 2
+def returnSoup(website):
+    page = requests.get(website)
+    return BeautifulSoup(page.content, 'html.parser')
 
 
-for game in gamesList:
-    print(game[0][0], " : ", game[1][0], " - ", game[0][1], " : ", game[1][1])
+def getCountriesWorldCup(soupNeeded):
+    countriesUnfiltered = soupNeeded.find_all('span', class_='nombre-equipo')
+    countriesFiltered = []
+    for country in countriesUnfiltered:
+        countriesFiltered.append(country.text)
+    return countriesFiltered
+
+
+def getResults(resultsSoup):
+    resultsUnfiltered = resultsSoup.find_all('a', class_='resultado')
+    resultsFiltered = []
+    resultsClean = []
+    for res in resultsUnfiltered:
+        resultsFiltered.append(res.text)
+
+    for r in resultsFiltered:
+        numbers = r.split()
+        numbers.remove(numbers[1])
+        if len(numbers) == 2:
+            resultsClean.append(numbers)
+    return resultsClean
+
+
+def matchResultCells(result, countries):
+    gamesList = []
+    teams = 0
+
+    for games in range(len(result)):
+        firstGame = []
+        firstGame.append(countries[teams:(teams+2)])
+        firstGame.append(result[games])
+        gamesList.append(firstGame)
+        teams = teams + 2
+
+    for game in gamesList:
+        print(game[0][0], " : ", game[1][0],
+              " - ", game[0][1], " : ", game[1][1])
+
+
+def main(webadress):
+    url = webadress
+
+    soup = returnSoup(url)
+    participants = getCountriesWorldCup(soup)
+    gamesResults = getResults(soup)
+
+    boards = matchResultCells(gamesResults, participants)
+
+
+main('https://resultados.as.com/resultados/futbol/mundial/2022/jornada/grupos_a_3/')
