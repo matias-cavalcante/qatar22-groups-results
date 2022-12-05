618b0016d3ec4660c27837d95800674573d3b07c
matias-cavalcante <matias.cavalcante93@gmail.com>
2022-12-05 01:35:17 +0000

    Information placed in automatically created divs

--- Script.js
+++ Script.js
@@ -7,10 +7,23 @@ button.addEventListener('click', function(){
     fetch('http://192.168.1.224:5000/')           //api for the get request
     .then(response => response.json())
     .then(data => {
-        frame.style.backgroundColor = "green";
-        for (let block = 0; block < 16; block ++){
+            //console.log(Object.keys(data[0])[1]);
+
+            //console.log( Object.values(data[0])[0] );
+        for (let block = 0; block < data.length; block ++){
             const box = document.createElement("div");
+            let firstTeam = Object.keys(data[block])[0];
+            let firstTeamGoals = Object.values(data[block])[0];
+            let secondTeam = Object.keys(data[block])[1];
+            let secondTeamGoals = Object.values(data[block])[1];
+            let match = firstTeam + " " + firstTeamGoals + " - " + secondTeam + " " + secondTeamGoals;
+            let matchDate = Object.values(data[block])[2]
+
             box.classList.add("div-few-style");
+            //box.innerText = Object.keys(data[block])[1];
+            box.innerText = match;
+            box.innerText = box.innerText + "\n" + matchDate;
+            box.style.color = "white";
             frame.appendChild(box);
         }
         }
