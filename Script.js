73d78f090dbbdfd932075dd07f491d965bf038e9
matias-cavalcante <matias.cavalcante93@gmail.com>
2022-12-07 00:43:35 +0000

    Flags added but not how intended. Need positioning

--- Script.js
+++ Script.js
@@ -13,17 +13,32 @@ firstButton.addEventListener('click', function(){
         if (resultDisplayed < 1){
             for (let block = 0; block < data.length; block ++){
                 const box = document.createElement("div");
+
                 let firstTeam = Object.keys(data[block])[0];
                 let firstTeamGoals = Object.values(data[block])[0];
                 let secondTeam = Object.keys(data[block])[1];
                 let secondTeamGoals = Object.values(data[block])[1];
                 let match = firstTeam + " " + firstTeamGoals + " - " + " " + secondTeamGoals + " " + secondTeam ;
-                let matchDate = Object.values(data[block])[2]
-    
+                let matchDate = Object.values(data[block])[2];
+
                 box.classList.add("div-few-style");
                 box.innerText = match;
                 box.innerText = box.innerText + "\n" + matchDate;
                 box.style.color = "#1B2430";
+
+                let flagLeft = document.createElement("img");
+                flagLeft.src = "downloadImages/" + firstTeam + ".png";
+                flagLeft.style.maxWidth = "50px";
+                flagLeft.style.maxHeight = "45px";
+                box.appendChild(flagLeft);
+            
+                
+                let flagRight = document.createElement("img");
+                flagRight.src = "downloadImages/" + secondTeam + ".png";
+                flagRight.style.maxWidth = "50px";
+                flagRight.style.maxHeight = "45px";
+                box.appendChild(flagRight); 
+                
                 frame.appendChild(box);
                 resultDisplayed++;
             }
