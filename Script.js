2fde5e6b8209c11c86160c80743923d031597ac2
matias-cavalcante <matias.cavalcante93@gmail.com>
2022-12-11 13:40:42 +0000

    Now the response from flask should be more like needed

--- Script.js
+++ Script.js
@@ -16,6 +16,7 @@ var resultDisplayed = 0;
 
 firstButton.addEventListener('click', function(){
     fetch('https://matiass37.pythonanywhere.com/')
+    /*fetch('http://127.0.0.1:5001/')*/
     .then(response => response.json())
     .then(data => {
         frame.appendChild(phaseContainer);
