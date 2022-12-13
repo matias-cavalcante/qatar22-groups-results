264ff40b43e66353dc7e1c2bb7347c00a4f15739
matias-cavalcante <matias.cavalcante93@gmail.com>
2022-12-13 00:19:07 +0000

    Media query modified for mobile

--- Script.js
+++ Script.js
@@ -15,10 +15,8 @@ var resultDisplayed = 0;
 
 
 firstButton.addEventListener('click', function(){
-    fetch('https://matiass37.pythonanywhere.com/', {
-        mode: 'no-cors'
-    })
-    /*fetch('http://127.0.0.1:5001/')*/
+    /*fetch('https://matiass37.pythonanywhere.com/')*/
+    fetch('http://127.0.0.1:5001/')
     .then(response => response.json())
     .then(data => {
         frame.appendChild(phaseContainer);
